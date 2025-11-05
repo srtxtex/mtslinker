f13d50461cb240b8e5a9956e74f705d2de8408bd
KarenKing <20784850+KARENKING112@users.noreply.github.com>
2025-11-05 21:49:03 +0300

    Improve error handling in fetch_webinar_data
    Refactor fetch_webinar_data to handle errors and validate data.

--- mtslinker/webinar.py
+++ mtslinker/webinar.py
@@ -8,22 +8,35 @@ from mtslinker.utils import create_directory_if_not_exists
 
 
 def fetch_webinar_data(event_sessions: str, record_id: str, session_id=None, max_duration=None):
-    json_data_url = construct_json_data_url(event_session_id=event_sessions, recording_id=record_id)
-    json_data = fetch_json_data(url=json_data_url, session_id=session_id)
-    
-    if not json_data:
-        logging.error('Failed to fetch webinar data. Check the session ID or URL.')
-        return
+    try:
+        json_data_url = construct_json_data_url(event_session_id=event_sessions, recording_id=record_id)
+        json_data = fetch_json_data(url=json_data_url, session_id=session_id)
+        
+        if not json_data:
+            logging.error('Failed to fetch webinar data. Check the session ID or URL.')
+            return False
 
-    sanitized_name = re.sub(r'[\s\/:*?"<>|]+', '_', json_data['name'])
-    directory = create_directory_if_not_exists(sanitized_name)
-    output_video_path = os.path.join(directory, f'{sanitized_name}.mp4')
+        if 'name' not in json_data:
+            logging.error('Invalid webinar data: missing name field.')
+            return False
 
-    total_duration, video_clips, audio_clips = process_video_clips(directory, json_data)
-    logging.info(
-        f'Downloaded and processed {len(video_clips) + len(audio_clips)} files ({total_duration} sec) for merging.')
+        sanitized_name = re.sub(r'[\s\/:*?"<>|]+', '_', json_data['name'])
+        directory = create_directory_if_not_exists(sanitized_name)
+        output_video_path = os.path.join(directory, f'{sanitized_name}.mp4')
 
-    compile_final_video(total_duration, video_clips, audio_clips, output_video_path, max_duration)
-    logging.info(f'Final video saved to {output_video_path}')
-    
-    return 1
+        total_duration, video_clips, audio_clips = process_video_clips(directory, json_data)
+        
+        if not video_clips and not audio_clips:
+            logging.error('No video or audio clips found to merge.')
+            return False
+        
+        logging.info(
+            f'Downloaded and processed {len(video_clips) + len(audio_clips)} files ({total_duration} sec) for merging.')
+
+        compile_final_video(total_duration, video_clips, audio_clips, output_video_path, max_duration)
+        logging.info(f'Final video saved to {output_video_path}')
+        
+        return True
+    except Exception as e:
+        logging.error(f'Failed to process webinar: {e}')
+        return False
